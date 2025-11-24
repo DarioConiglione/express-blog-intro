aa2fe8e203ee01dc038b469143d25617ed9ed64c
DarioConiglione <dariocng13@gmail.com>
2025-11-24 17:57:21 +0100

    fix array

--- app.js
+++ app.js
@@ -5,31 +5,32 @@ const port = 3000
 const posts = [
     {
         titolo: "Perché devi iniziare ad investire",
-        contenuto: "Questo contenuto devo ancora generarlo bene perché voglio capire prima come funziona la logica, poi penso a perfezionare", immagine: "/imgs/boolean.jpg",
+        contenuto: "Questo contenuto devo ancora generarlo bene perché voglio capire prima come funziona la logica, poi penso a perfezionare",
+        immagine: "public/images/ciambellone.jpg",
         tags: ["testo1", "testo2", "testo3", "testo4"]
     },
     {
         titolo: "Investire in obligazioni",
         contenuto: "Questo contenuto devo ancora generarlo bene perché voglio capire prima come funziona la logica, poi penso a perfezionare",
-        immagine: "/imgs/seconda.jpg",
+        immagine: "public/images/cracker_barbabietola.jpg",
         tags: ["tagA", "tagB", "tagC", "tagD"]
     },
     {
         titolo: "Investire in azioni",
         contenuto: "Questo contenuto devo ancora generarlo bene perché voglio capire prima come funziona la logica, poi penso a perfezionare",
-        immagine: "/imgs/terza.jpg",
+        immagine: "public/images/pane_fritto_dolce.jpg",
         tags: ["uno", "due", "tre", "quattro"]
     },
     {
         titolo: "Oro, materie prime ed altre asset class",
         contenuto: "Questo contenuto devo ancora generarlo bene perché voglio capire prima come funziona la logica, poi penso a perfezionare",
-        immagine: "/imgs/quarta.jpg",
+        immagine: "public/images/barbabietola.jpg",
         tags: ["alpha", "beta", "gamma", "delta"]
     },
     {
         titolo: "Come investire in ETF",
         contenuto: "Questo contenuto devo ancora generarlo bene perché voglio capire prima come funziona la logica, poi penso a perfezionare",
-        immagine: "/imgs/quinta.jpg",
+        immagine: "public/images/torta_paesana.jpg",
         tags: ["x", "y", "z", "w"]
     }
 ];
